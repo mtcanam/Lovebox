55344d663c19871b5387161f0305c859310c34a2
mtcanam <michael.canam@gmail.com>
2021-02-24 11:07:15 -0330

    Cleaned up setup and loop calls. Tested. Added reset servo function to prevent vibration stemming from fast motion.

--- message/message.ino
+++ message/message.ino
@@ -72,8 +72,15 @@ bool            isDisplayVisible        = false;
 
 /********************************* SERVO **********************************************/
 Servo myservo; 
+int startpos = 90;
 int pos = 90;
 int increment = -1;
+int ccw = 75;
+int cw = 105; 
+
+
+/********************************* MESSAGE ********************************************/
+bool unopened_message = false;
 
 
 /************************ START DRAW MESSAGE ******************************************/
@@ -86,14 +93,14 @@ void drawMessage(const String& message) {
   oled.setCursor(0,0);
   
   // change the text color to foreground color
-  oled.setTextColor(OLED_Text_Color);
+  oled.setTextColor(OLED_Color_White);
 
   // draw the new time value
   oled.print(message);
 }
 
 
-/********************************** START CONNECT*****************************************/
+/********************************** START WIFI ********************************************/
 void setup_wifi() {
 
   delay(10);
@@ -116,6 +123,34 @@ void setup_wifi() {
 }
 
 
+/********************************** START OTA ********************************************/
+void setup_ota() {
+  Serial.println("Setting up OTA.");
+  ArduinoOTA.setPort(OTAport);
+  ArduinoOTA.setHostname(SENSORNAME);
+  ArduinoOTA.setPassword((const char *)OTApassword);
+  ArduinoOTA.onStart([]() {
+    Serial.println("Starting");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA Setup Complete.");
+}
+
+
 /********************************** START CALLBACK*****************************************/
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
@@ -128,12 +163,15 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   message[length] = '\0';
   Serial.println(message);
+  unopened_message = true;
+  EEPROM.put(144, unopened_message);
   drawMessage(message);
 }
 
 
 /********************************** START SEND STATE*****************************************/
 void sendState() {
+  //May want to add this in the future to publish a message when the box is opened.
   /*
   StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
 
@@ -173,114 +211,126 @@ void reconnect() {
 }
 
 
-/********************************** START SPINSERVO*****************************************/
-void spinServo(){
-    myservo.write(pos);      
-    delay(50);    // Warte 50ms um den Servo zu drehen
+/********************************** START SPIN SERVO ***************************************/
+void spin_servo(){
+  //Write the new position to the servo
+  myservo.write(pos);      
+  //Wait 50ms for servo to actuate
+  delay(50);
+  //If we are at one of the two boundaries, then reverse the current increment to turn the other way
+  if(pos == ccw || pos == cw){
+    increment *= -1;
+  }
+  //Increment the position for next call
+  pos += increment;
+}
 
-    if(pos == 75 || pos == 105){ // Drehbereich zwischen 75°-105°
-      increment *= -1;
+
+/********************************** START RESET SERVO ***************************************/
+void reset_servo(){
+  //Read the current position from the servo
+  int currpos = myservo.read();      
+  while (currpos != startpos){
+    //Set the increment to work in the direction of the start position
+    if(currpos < startpos){
+      increment = 1;
+    }else{
+      increment = -1;
     }
-    pos += increment;
+    //Increment the position for next call
+    currpos += increment;
+    myservo.write(currpos);
+    //Wait 50ms for servo to actuate
+    delay(50);
+  }
+}
+
+/********************************** START GET LIGHT ****************************************/
+float get_light(){
+  //Get the light value as a percentage of max
+  float reading = analogRead(LS_PIN) / 1024.0 * 100.0;
+  return reading;
 }
 
 
 /********************************** START SETUP*****************************************/
 void setup() {
 
-  
-  myservo.attach(2);       // Servo an D0
-  myservo.write(0);
-
-  pinMode(LS_PIN,  INPUT);
-
-  // settling time
-  delay(250);
-  
+  //Set the serial baud rate
   Serial.begin(9600);
-  
   delay(10);
-
-  ArduinoOTA.setPort(OTAport);
-
-  ArduinoOTA.setHostname(SENSORNAME);
-
-  ArduinoOTA.setPassword((const char *)OTApassword);
-
   Serial.println("Starting Node named " + String(SENSORNAME));
 
+
+  //Set up the servo
+  myservo.attach(2);
+  myservo.write(90);
+
+  //Set up the light sensor
+  pinMode(LS_PIN,  INPUT);
+
+  //Allow sensors to settle
+  delay(250);
+  
+  //Set up the OTA
+  setup_ota();
+
+  //Set up WiFi
   setup_wifi();
 
+  //Set up MQTT
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 
-  ArduinoOTA.onStart([]() {
-    Serial.println("Starting");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
+  //Print success to serial
   Serial.println("Ready");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
   reconnect();
 
-  // initialise the SSD1331
+  //Set up the screen
+  Serial.println("Setting up screen");
   oled.begin();
   oled.setFont();
-  oled.fillScreen(OLED_Backround_Color);
-  oled.setTextColor(OLED_Text_Color);
+  oled.fillScreen(OLED_Color_Black);
+  oled.setTextColor(OLED_Color_White);
   oled.setTextSize(1);
-
-  // the display is now on
   isDisplayVisible = true;
+
+  //Set up EEPROM to maintain values when powered off
+  EEPROM.begin(512);
+  EEPROM.get(144, unopened_message);
+  Serial.println("Setting up EEPROM with a starting value of " + (String)unopened_message);
   
-  //EEPROM.begin(512);
-  //idSaved = EEPROM.get(142, idSaved);
-  //wasRead = EEPROM.get(144, wasRead);
-
-
 }
 
 
 /********************************** START LOOP*****************************************/
 void loop() {
-
+  //For OTA
   ArduinoOTA.handle();
-  
+
+  //For MQTT
   client.loop();
-  
+
+  //Make sure WiFi is connected
   if (WiFi.status() != WL_CONNECTED) {
     setup_wifi();
   }
 
-  float reading = analogRead(LS_PIN) / 1024.0 * 100.0; //Take brightness reading
-
-  Serial.println("The brightness reading is: " + (String)reading);
-  
-  delay(100);
-  
-  /*
-  while(!wasRead){   
-    spinServo();    // Drehe Herz
-    lightValue = analogRead(0);      // Lese Helligkeitswert
-    if(lightValue > 300) { 
-      wasRead = 1;
-      EEPROM.write(144, wasRead);
-      EEPROM.commit();
+  while (unopened_message){
+    //While there is an unopened message, spin the servo and check the light sensor
+    spin_servo();
+    float lightval = get_light();
+    if(lightval > 5.0){
+      //Box was opened. 
+      
+      Serial.println("Box Opened!");
+      reset_servo();
+      unopened_message = false;
+      EEPROM.put(144, unopened_message);
     }
   }
-  */
+  
+  delay(100);
 }
